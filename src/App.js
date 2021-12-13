f9d9a6373d084dfd2bb38a3d12f081d5670b51d3
SrinivasK <ksrinivas.ui@outlook.com>
2021-12-13 23:08:00 +0530

    Amplify push test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React with AWS
         </a>
       </header>
     </div>
